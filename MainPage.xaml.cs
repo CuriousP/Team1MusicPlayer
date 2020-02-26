cd7226be0557416679e6697d92e1dde0ee9d8ea9
deepjain73 <38891452+deepjain73@users.noreply.github.com>
2020-02-26 09:18:01 -0800

    updated code to show lable on where we are on screen and also fixed add/remove fav button.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -156,6 +156,7 @@ namespace Team1MusicPlayer
             mySearchBox.QueryText = string.Empty;
             songs.Clear();
             SongManager.GetAllSongs(songs);
+            SongTextBlock.Text = "All Songs";
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -195,42 +196,52 @@ namespace Team1MusicPlayer
         private void FavoriteButton_Click(object sender, RoutedEventArgs e)
         {            
             var button = sender as Button;
-            Windows.UI.Xaml.Media.SolidColorBrush sb = (Windows.UI.Xaml.Media.SolidColorBrush) button.Foreground;
-            bool bAddSongToFav = false;
-            if (sb.Color.R != 0)
-            {
-                button.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
-            }
-            else
-            {
-                button.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
-                bAddSongToFav = true;
-            }
-
+            //Windows.UI.Xaml.Media.SolidColorBrush sb = (Windows.UI.Xaml.Media.SolidColorBrush) button.Foreground;
+            //bool bAddSongToFav = false;
+            //if (sb.Color.R != 0)
+            //{
+            //    button.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
+            //}
+            //else
+            //{
+            //    button.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+            //    bAddSongToFav = true;
+            //}
+            //find out if song exists in fav list
             Song favSong = (Song)button.DataContext;
-            if (bAddSongToFav)
+            Song existingSong =SongManager.favoriteSongs.FirstOrDefault(s => s.AudioFile.Equals(favSong.AudioFile));
+           
+            if (existingSong == null)
             {
                 SongManager.AddFavoriteSong(favSong);
             }
             else
             {
                 SongManager.RemoveFavoriteSong(favSong);                                
-            }            
+            }
+            if (SongTextBlock.Text == "Favorite Songs")
+                SongManager.GetFavoriteSongs(songs);
+            else if (SongTextBlock.Text == "All Songs")
+                SongManager.GetAllSongs(songs);
+
         }
         private void Album1Button_Click(object sender, RoutedEventArgs e)
         {
             SongManager.FilterSongByAlbumName(songs, "alai");
+            SongTextBlock.Text = "Album1 Songs";
         }
 
         private void Album2Button_Click(object sender, RoutedEventArgs e)
         {
             SongManager.FilterSongByAlbumName(songs, "AEM");
+            SongTextBlock.Text = "Album2 Songs";
         }
 
         private void FavoritePlaylist_Click(object sender, RoutedEventArgs e)
         {
             songs.Clear();
             SongManager.GetFavoriteSongs(songs);
+            SongTextBlock.Text = "Favorite Songs";
            
         }
     }
